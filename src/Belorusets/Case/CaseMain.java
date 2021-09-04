094faa5ddd5e72fd69255235db3fc6210636c613
Dmitry Milov <milovdg@gmail.com>
2021-09-04 13:47:07 +0300

    04.09.2021-2

--- src/Belorusets/Case/CaseMain.java
+++ src/Belorusets/Case/CaseMain.java
@@ -4,15 +4,6 @@ public class CaseMain {
     public static void main(String[] args) {
         int x = 4;
 
-        System.out.println("==========The new style============");
-
-        switch (x){
-            case 0 -> System.out.println("Nothing is in there");
-            case 1, 2 -> System.out.println("Just a little bit");
-            case 3, 4 -> System.out.println("Something is in there");
-            default -> System.out.println("Default value");
-        }
-
         System.out.println("==========The old style============");
 
         switch (x){
@@ -26,8 +17,28 @@ public class CaseMain {
             case 3:
             case 4:
                 System.out.println("Something is in there");
+                break;
             default:
                 System.out.println("Default value");
         }
+
+        System.out.println("==========The new style============");
+
+        switch (x){
+            case 0 -> System.out.println("Nothing is in there");
+            case 1, 2 -> System.out.println("Just a little bit");
+            case 3, 4 -> System.out.println("Something is in there");
+            default -> System.out.println("Default value");
+        }
+
+//        ==========The new style2============
+
+        int s;
+        s = switch (x) {
+            case 0 -> 2;
+            default -> 3;
+        };
+
+
     }
 }
